e2c7ca0efc20a6978ff3f4cc719895dbd9c5c7ed
wangyingyu <635913935@qq.com>
2017-04-07 17:04:27 +0800

    你好世界

--- demo.js
+++ demo.js
@@ -1,29 +1,10 @@
-'use strict';
-//var names = ['Alice', 'Emily', 'Kate'];
-let names = {
-	"name": 1,
-	"age": "2",
-	"data": ['Alice', 'Emily', 'Kate']
-};
-
-var NotesList = React.createClass({
+var Reactdemo = React.CreateClass({
 	render: function() {
-		return (
-			<ol>
-				{
-					React.Children.map(this.props.children, function (child) {
-						return <li>{child}</li>;
-					})
-				}
-			</ol>
-		);
+		return (<h1>你好世界</h1>)
 	}
 });
-
-ReactDOM.render(
-	<NotesList>
-		<span>hello</span>
-		<span>world</span>
-	</NotesList>,
-	document.body
-);
+React.render(<Reactdemo/>,
+	document.getElementById("contanier"),
+	function() {
+		alert("ok")
+	})
